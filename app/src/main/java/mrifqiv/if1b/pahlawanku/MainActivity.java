8279628228fd5b9049c8d5c0d1eb7c57dfe6e411
Rifqi <rifqivirgiansyah@gmail.com>
2022-11-14 10:10:02 +0700

    1

---
+++ app/src/main/java/mrifqiv/if1b/pahlawanku/MainActivity.java
@@ -0,0 +1,14 @@
+package mrifqiv.if1b.pahlawanku;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
